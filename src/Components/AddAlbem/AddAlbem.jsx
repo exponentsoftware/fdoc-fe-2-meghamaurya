979ef923dc2078a4435077f180ed88c5a2653560
megha <meghamini40@gmail.com>
2022-11-27 00:36:42 +0530

    created input fields and add event listener

--- src/Components/AddAlbem/AddAlbem.jsx
+++ src/Components/AddAlbem/AddAlbem.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const AddAlbem = () => {
+  const [addAlbem, setAddAlbem] = useState("");
+  const [artist, setArtist] = useState("");
+  const [addCover, setAddCover] = useState("");
+
+  const handleSubmit = () => {};
   return (
     <div>
-      <label for="ablem_title">Albem Title</label>
-      <input />
-      <label for="artist">Artist</label>
-      <input />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="ablem_title">Albem Title</label>
+        <input value={addAlbem} onChange={(e) => setAddAlbem(e.target.value)} />
+        <label htmlFor="artist">Artist</label>
+        <input value={artist} onChange={(e) => setArtist(e.target.value)} />
+        <label htmlFor="albem_cover">upload Albem Cover</label>
+        <input value={addCover} onChange={(e) => setAddCover(e.target.value)} />
+      </form>
     </div>
   );
 };
