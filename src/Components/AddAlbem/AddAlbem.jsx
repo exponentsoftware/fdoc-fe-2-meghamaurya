5e831088415ad1233e14228da800f1200c6ea61f
megha <meghamini40@gmail.com>
2022-11-26 23:17:59 +0530

    initial commit

---
+++ src/Components/AddAlbem/AddAlbem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddAlbem = () => {
+  return (
+    <div>
+      <label for="ablem_title">Albem Title</label>
+      <input />
+      <label for="artist">Artist</label>
+      <input />
+    </div>
+  );
+};
+
+export default AddAlbem;
