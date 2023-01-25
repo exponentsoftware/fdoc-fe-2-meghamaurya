518e8059757562edec5c1f74305519aee256e6eb
megha <meghamini40@gmail.com>
2023-01-25 15:07:12 +0530

    created obj for new albem and api call to upload image

--- src/Components/AddAlbem/AddAlbem.jsx
+++ src/Components/AddAlbem/AddAlbem.jsx
@@ -1,20 +1,49 @@
 import React from "react";
 import { useState } from "react";
-
+import axios from "axios";
 const AddAlbem = () => {
-  const [addAlbem, setAddAlbem] = useState("");
-  const [artist, setArtist] = useState("");
-  const [addCover, setAddCover] = useState("");
+  const [state, setState] = useState({
+    artist: "",
+    albem: "",
+    cover: "",
+  });
+  const [addAlbem, setAddAlbem] = useState([]);
+  let id = 0;
 
-  const handleSubmit = () => {
-    const albems = [
-      {
-        id: "1",
-        artist: "A. R. Rehman",
-        albem: "",
-        cover: "",
-      },
-    ];
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://api.imgur.com/3/image",
+        {
+          cover: state.cover,
+        },
+        {
+          headers: {
+            Authorization: "Client-ID YOUR_CLIENT_ID",
+          },
+        }
+      )
+      .then((response) => {
+        let imageUrl = response.data.data.link;
+
+        let newAlbum = {
+          id: ++id,
+          artist: state.artist,
+          albem: state.albem,
+          cover: imageUrl,
+        };
+        setAddAlbem([...addAlbem, newAlbum]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div>
@@ -22,16 +51,16 @@ const AddAlbem = () => {
         <label htmlFor="ablem_title">Albem Title</label>
         <input
           type="text"
-          name="title"
+          name="albem"
           required="required"
           placeholder="Enter a title"
-          value={addAlbem}
-          onChange={(e) => setAddAlbem(e.target.value)}
+          onChange={handleChange}
         />
         <label htmlFor="artist">Artist</label>
-        <input value={artist} onChange={(e) => setArtist(e.target.value)} />
+        <input name="artist" onChange={handleChange} />
         <label htmlFor="albem_cover">upload Albem Cover</label>
-        <input value={addCover} onChange={(e) => setAddCover(e.target.value)} />
+        <input name="cover" onChange={handleChange} />
+        <button type="submit">Add Albem</button>
       </form>
     </div>
   );
